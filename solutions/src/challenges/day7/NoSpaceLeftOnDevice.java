2a68c80521eaa4fae8c31ff9619017fe36cd73b9
Damian Marek <zmdddamian@gmail.com>
2022-12-17 00:16:13 +0100

    day7 part 2 solution

--- solutions/src/challenges/day7/NoSpaceLeftOnDevice.java
+++ solutions/src/challenges/day7/NoSpaceLeftOnDevice.java
@@ -9,23 +9,58 @@ public class NoSpaceLeftOnDevice implements Solver<Integer> {
 
     private static final String ONLY_DIGITS_REGEX = "\\d+";
     private Dir currentDir;
+    private static final int TOTAL_DISC_SPACE = 70000000;
+    private static final int FREE_SPACE_NEEDED = 30000000;
+
+    private static boolean isElementToSkip(String prev, String element) {
+        return element.equals("$") || element.equals("cd") || element.equals("ls") || element.equals("dir")
+                || element.matches(ONLY_DIGITS_REGEX) || prev.equals("dir");
+    }
+
+    private static boolean isComebackToParent(String prev, String element) {
+        return prev.equals("cd") && element.equals("..");
+    }
+
+    private static int calculateResult(HashSet<Dir> dirs, Dir root) {
+        var result = TOTAL_DISC_SPACE;
+        if (root == null) {
+            throw new IllegalStateException();
+        }
+        var usedSpace = root.calculateSize();
+        for (var dir : dirs) {
+            final int size = dir.calculateSize();
+            if (FREE_SPACE_NEEDED <= TOTAL_DISC_SPACE - usedSpace + size && size < result) {
+                result = size;
+            }
+        }
+
+        return result;
+    }
 
     @Override
     public Integer solve() {
-        String prev = "";
         var dirs = new HashSet<Dir>();
-        for (var element : new FileLineIterator(7, "example")) {
-            if (element.equals("$") || element.equals("cd") || element.equals("ls") || element.equals("dir")
-                    || element.matches(ONLY_DIGITS_REGEX) || prev.equals("dir")) {
+        var root = createFileStructure(dirs);
+        return calculateResult(dirs, root);
+    }
+
+    private Dir createFileStructure(HashSet<Dir> dirs) {
+        String prev = "";
+        Dir root = null;
+        for (var element : new FileLineIterator(7, "input")) {
+            if (isElementToSkip(prev, element)) {
                 prev = element;
                 continue;
             }
 
-            if (prev.equals("cd") && element.equals("..")) {
+            if (isComebackToParent(prev, element)) {
                 currentDir = currentDir.getParent();
             } else if (prev.equals("cd")) {
                 var dir = new Dir(currentDir);
                 dirs.add(dir);
+                if (root == null) {
+                    root = dir;
+                }
                 if (this.currentDir != null) {
                     currentDir.addSubDir(dir);
                 }
@@ -37,15 +72,6 @@ public class NoSpaceLeftOnDevice implements Solver<Integer> {
 
             prev = element;
         }
-
-        var result = 0;
-        for (var dir : dirs) {
-            final int size = dir.calculateSize();
-            if (size < 100000) {
-                result += size;
-            }
-        }
-
-        return result;
+        return root;
     }
 }
